7e14321c40eae81c097408996c50caf36da96902
danny galimi <dannygalimi13@aim.com>
2020-09-01 20:56:35 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -2,7 +2,7 @@ require 'pry'
 
   # This is the array we will be passing into the remove_strawberry method
   # contacts = {
-  #   "Jon Snow" => {
+  #  "Jon Snow" => {
   #     name: "Jon",
   #     email: "jon_snow@thewall.we", 
   #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
@@ -15,7 +15,7 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-contacts.each do |[person, contact_details_hash|
+contacts.each do |person, contact_details_hash|
   binding.pry
 end
 end
